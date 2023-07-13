f331f5e5f942432b71124ee235ac8d29a84b97f0
Omar Heikal <omarheikal20@gmail.com>
2023-07-13 09:37:08 -0700

    SizeOff

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,10 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-printf("Size of a char: %ld bytes(s)\n", sizeof(char));
-printf("Size of an int: %ld bytes(s)\n", sizeof(int));
-printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
-printf("Size of a long long int: %ld bytes(s)\n", sizeof(long long int));
-printf("Size of a float: %ld bytes(s)\n", sizeof(float));
+{printf("Size of a char: %ld bytes(s)", sizeof(char));
+printf("Size of an int: %ld bytes(s)", sizeof(int));
+printf("Size of a long int: %ld bytes(s)", sizeof(long int));
+printf("Size of a long long int: %ld bytes(s)", sizeof(long long int));
+printf("Size of a float: %ld bytes(s)", sizeof(float));
 return (0);
 }
