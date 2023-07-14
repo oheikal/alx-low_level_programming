8069e65538c794f9b85b0138bb4921946cc6b777
Omar Heikal <omarheikal20@gmail.com>
2023-07-14 12:10:14 -0700

    Putchar alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  *main Entry - Point
+  *
+  * Return 0(Success)
+  */
+int main(void)
+{ int alphabet = 97;
+	for (alphabet; alphabet < 123; alphabet++)
+	{
+		putchar(alphabet);
+		putchar('\n');
+	}
+	return (0);
+}
